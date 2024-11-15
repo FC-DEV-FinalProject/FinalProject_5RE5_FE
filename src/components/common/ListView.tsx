b28b924d3108ff0feba3b76ae0727e0ad3477784
kimisadev27 <kimisadev27@gmail.com>
2024-11-15 12:02:05 +0900

    feat: 마이프로젝트 개발

---
+++ src/components/common/ListView.tsx
@@ -0,0 +1,149 @@
+import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { ROUTES } from '@/constants/route';
+import useChecked from '@/hooks/useChecked';
+import { IProjectProps } from '@/types/project';
+import { SquareArrowOutUpRightIcon } from 'lucide-react';
+import { MouseEvent, useState } from 'react';
+import { NavigateFunction, useNavigate } from 'react-router-dom';
+
+export type ViewOptionType = 'tile' | 'list';
+
+export interface IListViewProps {
+  option?: ViewOptionType;
+  data: IProjectProps[];
+  navi: NavigateFunction;
+}
+
+const ListView = ({ option = 'list', data }: Omit<IListViewProps, 'navi'>) => {
+  const navigate = useNavigate();
+  console.log('a');
+  return (
+    <div>
+      {option === 'list' ? (
+        <List data={data} navi={navigate} />
+      ) : (
+        <Tile data={data} navi={navigate} />
+      )}
+    </div>
+  );
+};
+
+const List = ({ data, navi }: IListViewProps) => {
+  const [items, setItems] = useState<IProjectProps[]>(data);
+  const { checkedList, handleCheckedList } = useChecked();
+
+  const handleCheckboxChange = (id: number): void => {
+    setItems(
+      items.map((item) => {
+        if (item.projectId === id) {
+          handleCheckedList.set(id);
+        }
+        return item.projectId === id
+          ? { ...item, checked: !item.checked }
+          : item;
+      })
+    );
+  };
+
+  const handleSelectAll = (event: MouseEvent<HTMLButtonElement>): void => {
+    setItems(
+      items.map((item) => ({
+        ...item,
+        checked: event.currentTarget.ariaChecked !== 'true',
+      }))
+    );
+    event.currentTarget.ariaChecked !== 'true'
+      ? handleCheckedList.addAll(items.map((item) => item.projectId))
+      : handleCheckedList.removeAll();
+  };
+
+  const onTest = () => {
+    alert(`삭제요청 리스트: ${checkedList}`);
+    // 삭제요청 API 호출
+  };
+
+  return (
+    <div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className='w-10'>
+              <Checkbox onClick={handleSelectAll} />
+            </TableHead>
+            <TableHead className='w-[50%]'>Name</TableHead>
+            <TableHead>Last modified</TableHead>
+            <TableHead>Created</TableHead>
+            <TableHead>바로가기</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {items.map((item, idx) => {
+            return (
+              <TableRow key={item.name + idx}>
+                <TableCell>
+                  <Checkbox
+                    onClick={() => {
+                      handleCheckboxChange(item.projectId);
+                    }}
+                    checked={item.checked}
+                  />
+                </TableCell>
+                <TableCell className='font-medium'>{item.name}</TableCell>
+                <TableCell>{item.modDate}</TableCell>
+                <TableCell>{item.regDate}</TableCell>
+                <TableCell
+                  className='hover:cursor-pointer'
+                  onClick={() => {
+                    navi(ROUTES.PROJECT + ROUTES.TTS + `/${item.projectId}`);
+                  }}
+                >
+                  <SquareArrowOutUpRightIcon />
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+      {checkedList.length > 0 ? (
+        <Button variant={'destructive'} onClick={onTest}>
+          삭제하기
+        </Button>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
+
+const Tile = ({ data, navi }: IListViewProps) => {
+  return (
+    <div>
+      <ul className='flex flex-wrap flex-1'>
+        {data.map((item, idx) => (
+          <li
+            key={item.name + idx}
+            className='p-5 m-5 w-[30%] border hover:cursor-pointer hover:scale-95 duration-100 rounded-lg z-0'
+            onClick={() => {
+              navi(ROUTES.PROJECT + ROUTES.TTS + `/${item.projectId}`);
+            }}
+          >
+            <Checkbox className='fixed z-10' />
+            <div className='content-center h-40 text-center'>썸네일</div>
+            <div>{item.name}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ListView;
--- src/pages/MyProject.tsx
+++ src/pages/MyProject.tsx
@@ -1,5 +1,58 @@
+import ListView, { ViewOptionType } from '@/components/common/ListView';
+import { Button } from '@/components/ui/button';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { PROJECT_DATA } from '@/mock/project';
+import { AlignJustifyIcon, LayoutGridIcon } from 'lucide-react';
+import { useState } from 'react';
+
 const MyProject = () => {
-  return <div>마이프로젝트입니다</div>;
+  const [viewOption, setViewOption] = useState<ViewOptionType>('list');
+  return (
+    <div className='w-[100%]'>
+      <div className='flex justify-between p-10 '>
+        <div className='selectDiv'>
+          <Select>
+            <SelectTrigger className='w-[100px]'>
+              <SelectValue placeholder='정렬' />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value='lastview'>최신 순</SelectItem>
+              <SelectItem value='dark'>순서2</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className='iconMenu'>
+          <Button
+            variant={viewOption === 'list' ? 'active' : 'ghost'}
+            size='icon'
+            onClick={() => {
+              setViewOption('list');
+            }}
+          >
+            <AlignJustifyIcon />
+          </Button>
+          <Button
+            variant={viewOption === 'tile' ? 'active' : 'ghost'}
+            size='icon'
+            onClick={() => {
+              setViewOption('tile');
+            }}
+          >
+            <LayoutGridIcon />
+          </Button>
+        </div>
+      </div>
+      <div className='p-10 pt-0 tableDiv'>
+        <ListView option={viewOption} data={PROJECT_DATA} />
+      </div>
+    </div>
+  );
 };
 
 export default MyProject;
