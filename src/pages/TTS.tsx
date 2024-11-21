3bb18e08824fdfa0d62f97002de274aece30d6ec
wonjichoe <wonjichoe@gmail.com>
2024-11-21 12:31:23 +0900

    feat: 상단 옵션 표시 버튼 추가(#29)

--- src/pages/TTS.tsx
+++ src/pages/TTS.tsx
@@ -3,6 +3,8 @@ import { Input } from '@/components/ui/input';
 import { Checkbox } from '@/components/ui/checkbox';
 import { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue,  } from '@/components/ui/select';
+import { Repeat2 } from 'lucide-react';
 
 interface TextInput {
   id: number;
@@ -34,12 +36,10 @@ const TTS = () => {
   const addTextInput = () => {
     const newId = textInputs.length > 0 ? Math.max(...textInputs.map(input => input.id)) + 1 : 1;
     setTextInputs([...textInputs, {id: newId, text: '', isSelected: false, isEditing: false }]);
-    // setEditingId(newId);
   };
 
   const handleTextChange = (id: number, newText: string) => {
     setTextInputs(textInputs.map(input => input.id === id ? {...input, text: newText } : input ));
-    // setEditingId(id);
     if (editingId === null) {
       setEditingId(id);
     }
@@ -60,21 +60,10 @@ const TTS = () => {
     setIsAllSelected(false);
   };
 
-  // const saveInput = (id: number) => {
-  //   setTextInputs(textInputs.map(input => input.id === id ? { ...input, isEditing: false } : input));
-  // };
   const saveInput = () => {
     setEditingId(null);
   };
 
-  // const cancelEdit = (id: number) => {
-  //   setTextInputs(textInputs.map(input => 
-  //     input.id === id ? { ...input, text: '', isEditing: false } : input
-  //   ));
-  //   if (inputRefs.current[id]) {
-  //     inputRefs.current[id]?.blur();
-  //   }
-  // };
   const cancelEdit = () => {
     setTextInputs(textInputs.map(input => 
       input.id === editingId ? { ...input, text: '' } : input
@@ -115,6 +104,28 @@ const TTS = () => {
             선택 삭제
           </Button>
         </div>
+        <div className="flex items-center justify-between mb-4">
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="성우이름" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="option1">성우 1</SelectItem>
+              <SelectItem value="option2">성우 2</SelectItem>
+              <SelectItem value="option3">성우 3</SelectItem>
+            </SelectContent>
+          </Select>
+          <Button variant="secondary">효과 라디오</Button>
+          <div className="flex justify-between">
+            <Button variant="secondary">음량</Button>
+            <Button variant="secondary">속도</Button>
+            <Button variant="secondary">높이</Button>
+            <Button variant="secondary">초기화</Button>
+            <Button>
+              <Repeat2 />재생성
+            </Button>
+          </div>
+        </div>
         {textInputs.map((input) => (
           <div key={input.id} className="flex items-center mb-2 space-x-2">
             <Checkbox 
@@ -139,7 +150,7 @@ const TTS = () => {
       <div className="text-center">
         {editingId !== null ? (
           <>
-          <Button onClick={saveInput}>저장</Button>
+          <Button onClick={saveInput} className="mr-1">저장</Button>
           <Button onClick={cancelEdit}>취소</Button>
           </>
         ) : (
