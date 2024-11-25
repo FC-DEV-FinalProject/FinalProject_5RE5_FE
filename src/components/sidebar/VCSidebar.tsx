491e17d073f532d24fe9b6fe6ce023c07a6d4240
kimisadev27 <kimisadev27@gmail.com>
2024-11-25 12:05:52 +0900

    feat: VC 사이드바 와이어프레임 적용

--- src/components/sidebar/VCSidebar.tsx
+++ src/components/sidebar/VCSidebar.tsx
@@ -1,8 +1,26 @@
+import { Button } from '@/components/ui/button';
+import { UploadIcon } from 'lucide-react';
+
 const VCSidebar = () => {
   return (
     <div className='flex flex-col'>
-      <h2 className='mb-2 text-lg font-bold'>VC 설정</h2>
-      <p>여기에는 VC 관련 설정 내용이 들어갑니다.</p>
+      <div className='pt-5 pb-5 border-b-[1px] border-b-gray-500'>
+        <p className='pb-5 '>삽입</p>
+        <Button variant={'default'} className='w-[100%]'>
+          보이스 클립 추가
+        </Button>
+      </div>
+      <div className='pt-5 pb-5 '>
+        <p className='pb-5 '>수정</p>
+        <Button variant={'default'} className='w-[100%]'>
+          생성 보이스 추가
+        </Button>
+      </div>
+      <div className='pt-5 pb-5 '>
+        <Button variant={'default'} className='w-[100%] '>
+          <UploadIcon /> 텍스트 파일 추가
+        </Button>
+      </div>
     </div>
   );
 };
