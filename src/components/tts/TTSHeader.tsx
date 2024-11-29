0b2152d2eca41e3f781c8a6ff0cce44f471702e2
kimisadev27 <34756233+kimisadev27@users.noreply.github.com>
2024-11-29 10:53:14 +0900

    [fix]: 배포 api endPoint 등록 (#64)
    * fix: TTSHeader.tsx 버튼 스타일 오류 수정(#30)
    
    * fix: 버그 수정 (#63)
    
    * fix: 배포 api endPoint 등록 (#63)
    
    ---------
    
    Co-authored-by: wonjichoe <wonjichoe@gmail.com>
    Co-authored-by: Haeun <haruyam15@gmail.com>

--- src/components/tts/TTSHeader.tsx
+++ src/components/tts/TTSHeader.tsx
@@ -26,8 +26,8 @@ export const TTSHeader: React.FC<TTSHeaderProps> = ({
       <div className="flex items-center space-x-4">
         <span className="text-gray-500">{new Date().toLocaleDateString()}</span>
         <div className="flex">
-          <Button type="submit" variant="aipark" className="w-[130px] mr-1 rounded-3xl">저장</Button>
-          <Button type="button" variant="aipark" className="w-[130px] rounded-3xl">전체 다운로드</Button>
+          <Button type="submit" variant="green" className="w-[130px] mr-1 rounded-3xl">저장</Button>
+          <Button type="button" variant="green" className="w-[130px] rounded-3xl">전체 다운로드</Button>
         </div>
       </div>
     </div>
