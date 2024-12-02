e7c6f67fbd7b7d801fdf04ebb6dfa5afe06dc448
Sonseongoh <nobleser@naver.com>
2024-12-03 00:43:51 +0900

    feat:popover api 적용 (#73)

--- src/apis/ttsVoice.ts
+++ src/apis/ttsVoice.ts
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import apiClient from '@/apis/apiClient';
 
 interface Language {
@@ -15,12 +14,34 @@ interface LanguageResponse {
   };
 }
 
-export const ttsVoice = async (): Promise<Language[]> => {
+export const ttsLanguage = async (): Promise<Language[]> => {
   try {
     const response = await apiClient.get<LanguageResponse>('/language');
     return response.data.response.languageList;
   } catch (error) {
     console.error(error);
     throw new Error('언어 데이터를 가져오는 데 실패했습니다.');
+  }
+};
+
+export const ttsStyle = async () => {
+  try {
+    const response = await apiClient.get('/style');
+    return response.data.response.styleList;
+  } catch (error) {
+    console.error(error);
+    throw new Error('스타일 데이터를 가져오는 데 실패했습니다.');
+  }
+};
+
+export const ttsVoiceName = async (languageCode: string, styleName: string) => {
+  try {
+    const response = await apiClient.get(
+      `/voice?languagecode=${languageCode}&stylename=${styleName}`
+    );
+    return response.data.response.voiceList; // 필요한 데이터 반환
+  } catch (error) {
+    console.error(error);
+    throw new Error('voiceName 데이터를 가져오는 데 실패했습니다.');
   }
 };
