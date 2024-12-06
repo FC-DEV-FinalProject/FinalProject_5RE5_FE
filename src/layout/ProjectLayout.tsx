9986e1ebc78de836ed4f8c9fcfb564f463eab626
Haeun <haruyam15@gmail.com>
2024-12-06 12:08:20 +0900

    feature: 로그인 화면 꽉차게 수정 (#92)

--- src/layout/ProjectLayout.tsx
+++ src/layout/ProjectLayout.tsx
@@ -59,76 +59,78 @@ const ProjectLayout = () => {
   }
 
   return (
-    <div className='flex flex-col h-screen'>
-      {/* 헤더 */}
-      <Header projectTab={true} />
+    <div className='max-w-[1280px] mx-auto'>
+      <div className='flex flex-col h-screen'>
+        {/* 헤더 */}
+        <Header projectTab={true} />
 
-      <div className='flex overflow-hidden grow'>
-        {/* 좌측 사이드바 */}
-        <aside className='p-4 w-[90px] flex-none border-r border-gray-200'>
-          <ul>
-            <li className='mb-2'>
-              <ProjectIconMenu
-                linkTo={`${ROUTES.PROJECT}${ROUTES.TTS}/${projectId}`}
-                name='TTS'
-                icon={BookAIcon}
-              />
-            </li>
-            <li className='mb-2'>
-              <ProjectIconMenu
-                linkTo={`${ROUTES.PROJECT}${ROUTES.VC}/${projectId}`}
-                name='VC'
-                icon={MicIcon}
-              />
-            </li>
-            <li className='mb-2'>
-              <ProjectIconMenu
-                linkTo={`${ROUTES.PROJECT}${ROUTES.CONCAT}/${projectId}`}
-                name='CONCAT'
-                icon={CombineIcon}
-              />
-            </li>
-          </ul>
-        </aside>
+        <div className='flex overflow-hidden grow'>
+          {/* 좌측 사이드바 */}
+          <aside className='p-4 w-[90px] flex-none border-r border-gray-200'>
+            <ul>
+              <li className='mb-2'>
+                <ProjectIconMenu
+                  linkTo={`${ROUTES.PROJECT}${ROUTES.TTS}/${projectId}`}
+                  name='TTS'
+                  icon={BookAIcon}
+                />
+              </li>
+              <li className='mb-2'>
+                <ProjectIconMenu
+                  linkTo={`${ROUTES.PROJECT}${ROUTES.VC}/${projectId}`}
+                  name='VC'
+                  icon={MicIcon}
+                />
+              </li>
+              <li className='mb-2'>
+                <ProjectIconMenu
+                  linkTo={`${ROUTES.PROJECT}${ROUTES.CONCAT}/${projectId}`}
+                  name='CONCAT'
+                  icon={CombineIcon}
+                />
+              </li>
+            </ul>
+          </aside>
 
-        <div className='grow w-[calc(100vw-90px)] flex h-full'>
-          {selectedMenu === 'concat' ? (
-            <ConcatLayout />
-          ) : (
-            <>
-              {/* 메인 콘텐츠 */}
-              <div className='overflow-auto grow'>
-                <main className='p-4 bg-white min-w-[800px]'>
-                  {ContentComponent}
-                </main>
-              </div>
+          <div className='grow w-[calc(100vw-90px)] flex h-full'>
+            {selectedMenu === 'concat' ? (
+              <ConcatLayout />
+            ) : (
+              <>
+                {/* 메인 콘텐츠 */}
+                <div className='overflow-auto grow'>
+                  <main className='p-4 bg-white min-w-[800px]'>
+                    {ContentComponent}
+                  </main>
+                </div>
 
-              {/* 우측 사이드바 */}
-              <aside className='p-4 w-[280px] flex flex-col border-l flex-none'>
-                {SidebarComponent}
-              </aside>
-            </>
-          )}
+                {/* 우측 사이드바 */}
+                <aside className='p-4 w-[280px] flex flex-col border-l flex-none'>
+                  {SidebarComponent}
+                </aside>
+              </>
+            )}
+          </div>
         </div>
-      </div>
 
-      {/* 푸터 */}
-      <footer
-        className={`flex flex-none items-center justify-center relative ${
-          isFooterExpanded ? 'h-[310px]' : 'h-[114px]'
-        } text-white border-t  transition-all duration-300`}
-      >
-        {/* 재생 버튼 텍스트 */}
-        <p className='text-lg text-black'>재생 버튼 들어갈곳</p>
-
-        {/* 아이콘 버튼 */}
-        <button
-          onClick={toggleFooter}
-          className='absolute bg-gray-600 rounded right-10 hover:bg-gray-700'
+        {/* 푸터 */}
+        <footer
+          className={`flex flex-none items-center justify-center relative ${
+            isFooterExpanded ? 'h-[310px]' : 'h-[114px]'
+          } text-white border-t  transition-all duration-300`}
         >
-          {isFooterExpanded ? <ChevronDown /> : <ChevronUp />}
-        </button>
-      </footer>
+          {/* 재생 버튼 텍스트 */}
+          <p className='text-lg text-black'>재생 버튼 들어갈곳</p>
+
+          {/* 아이콘 버튼 */}
+          <button
+            onClick={toggleFooter}
+            className='absolute bg-gray-600 rounded right-10 hover:bg-gray-700'
+          >
+            {isFooterExpanded ? <ChevronDown /> : <ChevronUp />}
+          </button>
+        </footer>
+      </div>
     </div>
   );
 };
