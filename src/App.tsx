ffe309d59e8dd31d720253585a38be97ac7b41f1
Haeun <haruyam15@gmail.com>
2024-12-06 10:18:50 +0900

    feature: 로그인 배경확장 버그로 제거, 추후개발 예정(#89)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useMemo } from 'react';
-import { getWrapBg } from '@/utils/ui';
 
 const queryClient = new QueryClient();
 
 function App() {
-  const bg = getWrapBg();
-
   return (
-    <div id='wrap' style={bg || undefined}>
-      <div className='max-w-[1280px] mx-auto'>
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-        </QueryClientProvider>
-      </div>
+    <div className='max-w-[1280px] mx-auto'>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </div>
   );
 }
--- src/utils/ui.ts
+++
@@ -1,7 +0,0 @@
-export const getWrapBg = (): React.CSSProperties | null => {
-  const pathName = window.location.pathname.toLocaleLowerCase();
-  const isSignin = pathName === '/signin';
-  return isSignin
-    ? { background: 'linear-gradient(to right, #D6EEFE 50%, #fff 50%)' }
-    : null;
-};
