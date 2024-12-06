5282781951a45ed25ab062b9afb8bcd332ef1713
Haeun <haruyam15@gmail.com>
2024-12-06 09:23:22 +0900

    feature : 로그인 배경색 꽉차게 수정 (#89)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useMemo } from 'react';
+import { getWrapBg } from '@/utils/ui';
 
 const queryClient = new QueryClient();
 
 function App() {
+  const bg = useMemo(() => getWrapBg(), []);
+
   return (
-    <div className='max-w-[1280px] mx-auto'>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
+    <div id='wrap' style={bg || undefined}>
+      <div className='max-w-[1280px] mx-auto'>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </div>
     </div>
   );
 }
